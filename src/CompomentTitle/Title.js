43279ef8dcfc3c2a3cef974211dcf09af0c50c43
nquochuy1995 <nquochuy1995@gmail.com>
2019-11-18 16:52:00 +0900

    Make a new Compoment title

---
+++ src/CompomentTitle/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Title extends React.Component {
+    render() {
+        return(
+            
+                <h1 className="text-center doubleUnderline">タイムシート(設計開発部)</h1>
+            
+        )
+    }
+}
+export default Title;
