fcf2d59f5bca703a33ecc4298ff06a22ff7bbc14
nquochuy <nquochuy1995@gmail.com>
2020-02-04 23:53:26 +0900

    Add express moudule firebase

--- functions/index.js
+++ functions/index.js
@@ -11,40 +11,92 @@ admin.initializeApp({
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello word");
-});
+const express = require('express');
+const app = express();
 
-exports.getScream = functions.https.onRequest((req, res) => {
-    //response.send("Hello word");
-    admin.firestore().collection('scream').get()
+app.get('/scream',(req , res) =>{
+    admin
+        .firestore()
+        .collection('scream')
+        .orderBy('createAt','desc')
+        .get()
         .then(data => {
             let screams = [];
-            data.forEach( doc => {
-                screams.push(doc.data());
+            data.forEach((doc) => {
+                screams.push({
+                    screamsId : doc.id,
+                    body : doc.data().body,
+                    userHandle : doc.data().userHandle,
+                    createdAt : doc.data().createAt // gan tên trường mới : tên trương ở firebase
+                });
             });
             return res.json(screams);
         })
-        .catch(err => console.error(err));
+        .catch((err) => console.error(err));
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
-    //response.send("Hello word");
+app.post('/scream',(req , res) =>{
     const newScream = {
         body : req.body.body,
         userHandle : req.body.userHandle, 
-        createAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin
     .firestore()
-        .collection('scream')
+        .collection('scream') // ten trong trường database 
         .add(newScream)
         .then(doc =>{
             res.json({message: `document ${doc.id} created successfully`});
         })
-        .catch(err =>{
-            res.status(500).json({ error : 'Something went wrong'});
+        .catch(err =>{ // catch để bat loi va hien thi message
+            res.status(500).json({ error : 'Something went wrong' });
             console.error(err);
-        })
+        });
 });
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello word");
+// });
+
+// exports.getScream = functions.https.onRequest((req, res) => { // get data về postman 
+//     //response.send("Hello word");
+//     admin
+//         .firestore()
+//         .collection('scream')
+//         .get()
+//         .then(data => {
+//             let screams = [];
+//             data.forEach( doc => {
+//                 screams.push(doc.data());
+//             });
+//             return res.json(screams);
+//         })
+//         .catch((err) => console.error(err));
+// });
+
+// exports.createScream = functions.https.onRequest((req, res) => { // post data về postman
+//     //response.send("Hello word");
+//     if (req.method !== 'POST') {
+//         return res.status(400).json({error : 'Method not allowed'});
+//     }
+//     const newScream = {
+//         body : req.body.body,
+//         userHandle : req.body.userHandle, 
+//         createAt: admin.firestore.Timestamp.fromDate(new Date())
+//     };
+
+//     admin
+//     .firestore()
+//         .collection('scream') // ten trong trường database 
+//         .add(newScream)
+//         .then(doc =>{
+//             res.json({message: `document ${doc.id} created successfully`});
+//         })
+//         .catch(err =>{ // catch để bat loi va hien thi message
+//             res.status(500).json({ error : 'Something went wrong' });
+//             console.error(err);
+//         });
+// });
+// https://baseurl.com/api/
+exports.api = functions.https.onRequest(app);
